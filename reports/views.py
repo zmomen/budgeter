ec011f5460b3a01e45187b15e0e04bd84e855019
Zaid Al-Momen <zalmomen01@gmail.com>
2019-02-05 15:38:58 -0600

    - created a TransactionManager in the transaction.models file. - moved all reports queries into manager method. This includes: reports for month details, spend by category, deposits, etc.
    That way, the model manager is separated from the reports/views.py which handles display to the HTML reports file.

--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,3 @@
-from django.db.models import functions, Sum
 from django.shortcuts import render
 
 from transactions.models import Transaction, Category
@@ -8,44 +7,23 @@ from transactions.models import Transaction, Category
 def reports(request, year=None, month=None, cat_id=None):
     # by month and year
     if year and month:
-        month_id_spend = Transaction.objects.values('tran_dt', 'tran_desc', 'category__name') \
-            .exclude(category__name__in=['Deposit', 'Credit Card']) \
-            .annotate(year=functions.ExtractYear('tran_dt')) \
-            .annotate(month=functions.ExtractMonth('tran_dt')) \
-            .annotate(totals=Sum('tran_amt')) \
-            .filter(year=year, month=month) \
-            .values('tran_dt', 'tran_desc', 'category__name', 'totals') \
-            .order_by('-tran_dt', 'category__name', '-totals')
+        month_id_spend = Transaction.objects.get_month_details(year=year, month=month)
 
         args = {'month_id_spend': month_id_spend, 'report_type': 'by month', 'monthYear': [month, year]}
         return render(request, 'reports/report_by_id.html', args)
+
     # else by category_id
     elif cat_id:
-        cat_id_spend = Transaction.objects.values('tran_dt', 'tran_type', 'tran_desc', 'category__name',
-                                                  'category__id') \
-            .annotate(totals=Sum('tran_amt')) \
-            .filter(category__id=cat_id) \
-            .values('tran_dt', 'tran_type', 'tran_desc', 'category__name', 'totals') \
-            .order_by('-tran_dt', 'category__name', '-totals')
+        cat_id_spend = Transaction.objects.get_by_cat_id(cat_id=cat_id)
         category_name = Category.objects.get(id=cat_id).name
+
         args = {'cat_id_spend': cat_id_spend, 'report_type': 'by category', 'category_name': category_name}
         return render(request, 'reports/report_by_id.html', args)
     # else main page.
     else:
-        cat_spend = Transaction.objects.values('category__name', 'category__id') \
-            .annotate(totals=Sum('tran_amt')) \
-            .order_by('-totals', 'category__name')
-
-        month_spend = Transaction.objects.annotate(year=functions.ExtractYear('tran_dt')) \
-            .exclude(category__name__in=['Deposit', 'Credit Card']) \
-            .annotate(month=functions.ExtractMonth('tran_dt')).values('year', 'month') \
-            .annotate(totals=Sum('tran_amt')).order_by('-year', '-month', '-totals')
-
-        deposits = Transaction.objects.filter(tran_type=2) \
-            .annotate(year=functions.ExtractYear('tran_dt')) \
-            .annotate(month=functions.ExtractMonth('tran_dt')).values('year', 'month') \
-            .annotate(totals=Sum('tran_amt')).order_by('-year', '-month', '-totals')
+        cat_spend = Transaction.objects.get_cat_spend()
+        month_spend = Transaction.objects.display_by_month_year()
+        deposits = Transaction.objects.get_deposit_trans()
 
         args = {'cat_spend': cat_spend, 'month_spend': month_spend, 'deposits': deposits}
-
         return render(request, 'reports/reports.html', args)
