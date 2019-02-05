02290752d2b53b774811fcd3d9dab565ea1bd289
Zaid Al-Momen <zmomen@andrew.cmu.edu>
2019-02-04 22:20:26 -0600

    changed the urls to point to the reports app.

---
+++ reports/urls.py
@@ -0,0 +1,11 @@
+# reports/urls.py
+from django.conf.urls import url
+
+from reports import views
+
+app_name = 'reports'
+urlpatterns = [
+    url(r'^reports/$', views.reports, name='reports'),
+    url(r'^reports/(?P<year>\d+)/(?P<month>\d+)/$', views.reports, name='by_month_id'),
+    url(r'^reports/category/(?P<cat_id>\d+)/$', views.reports, name='by_category_id'),
+]
