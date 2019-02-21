d2859b17ca5a42e54f0011d33d9ae80614f13890
zmomen <zmomen@andrew.cmu.edu>
2019-02-21 17:25:32 -0600

    Update HTML for search box

--- reports/urls.py
+++ reports/urls.py
@@ -7,5 +7,6 @@ app_name = 'reports'
 urlpatterns = [
     url(r'^reports/$', views.reports, name='reports'),
     url(r'^reports/(?P<year>\d+)/(?P<month>\d+)/$', views.reports, name='by_month_id'),
+    # url(r'^reports/(?P<year>\d+)/(?P<month>\d+)/$', views.income_reports, name='income'),
     url(r'^reports/category/(?P<cat_id>\d+)/$', views.reports, name='by_category_id'),
 ]
